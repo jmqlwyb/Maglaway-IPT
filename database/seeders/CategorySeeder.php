0abb4da38a485fbc10111156f0b4e1b0a871221d
jmqlwyb <maglawayb@gmail.com>
2025-02-03 13:08:26 +0800

    commit

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -11,9 +11,6 @@ class CategorySeeder extends Seeder {
             ['category_name' => 'Canned Goods', 'description' => 'Various canned food items'],
             ['category_name' => 'Beverages', 'description' => 'Drinks and beverages'],
             ['category_name' => 'Snacks', 'description' => 'Chips, cookies, and other snacks'],
-            ['category_name' => 'Dairy', 'description' => 'Milk, cheese, and other dairy products'],
-            ['category_name' => 'Frozen Foods', 'description' => 'Frozen meals and vegetables'],
-            ['category_name' => 'Household Essentials', 'description' => 'Basic household necessities'],
             ['category_name' => 'Pet Supplies', 'description' => 'Food and accessories for pets'],
             ['category_name' => 'Cleaning Supplies', 'description' => 'Detergents, disinfectants, and cleaning tools'],
         ];
