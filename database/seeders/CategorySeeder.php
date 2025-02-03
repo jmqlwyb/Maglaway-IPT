1a6dd6e8759da4bafbe1271fc3f1cbbf935ca6eb
jmqlwyb <maglawayb@gmail.com>
2025-02-03 13:34:59 +0800

    commit

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -9,7 +9,6 @@ class CategorySeeder extends Seeder {
     public function run() {
         $categories = [
             ['category_name' => 'Canned Goods', 'description' => 'Various canned food items'],
-            ['category_name' => 'Beverages', 'description' => 'Drinks and beverages'],
             ['category_name' => 'Snacks', 'description' => 'Chips, cookies, and other snacks'],
             ['category_name' => 'Pet Supplies', 'description' => 'Food and accessories for pets'],
             ['category_name' => 'Cleaning Supplies', 'description' => 'Detergents, disinfectants, and cleaning tools'],
