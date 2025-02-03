8a489d514f3d018bd5f0b4f7fc5283030369921d
jmqlwyb <maglawayb@gmail.com>
2025-02-03 13:32:46 +0800

    commit

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -13,6 +13,7 @@ class CategorySeeder extends Seeder {
             ['category_name' => 'Snacks', 'description' => 'Chips, cookies, and other snacks'],
             ['category_name' => 'Pet Supplies', 'description' => 'Food and accessories for pets'],
             ['category_name' => 'Cleaning Supplies', 'description' => 'Detergents, disinfectants, and cleaning tools'],
+            ['category_name' => 'Apparel', 'description' => 'T-shirts, clothing, and fashion items'],
         ];
 
         Category::insert($categories);
